382b7a5b86b51fd358bd1616245df3409c9053bb
yue.fan <yue.fan@thoughtworks.com>
2020-11-10 19:09:03 -0500

    solve practice 1

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,7 @@
+import { getURL } from "./utils";
+import { REGION } from "./constant";
 
+const url = getURL();
+const region = REGION;
+
+export { url, region };
--- src/practice1/utils.js
+++ src/practice1/utils.js
@@ -1,1 +1,5 @@
-const getURL
+import { ADDRESS, PORT, PATH } from "./constant";
+
+export const getURL = () => {
+  return `${ADDRESS}:${PORT}${PATH}`;
+};
