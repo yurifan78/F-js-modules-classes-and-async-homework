1786f1dcbf4d9908e015ceb92130baea983af580
yue.fan <yue.fan@thoughtworks.com>
2020-11-13 03:34:51 -0500

    fix practice 3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,13 +1,10 @@
 import fetch from "node-fetch";
 
 export const getPoetry = () => {
-  // fetch(poetry);
-  return fetch("https://v1.jinrishici.com/all.json").then((response) =>
-    response
-      .json()
-      .then((data) => {
-        return [data.origin, data.author, data.content];
-      })
-      .catch(console.error)
-  );
+  return fetch("https://v1.jinrishici.com/all.json")
+    .then((response) => response.json())
+    .then((data) => {
+      return [data.origin, data.author, data.content];
+    })
+    .catch(console.error);
 };
