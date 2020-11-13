e5c479da9a52906869662280a11070fddb14d325
yue.fan <yue.fan@thoughtworks.com>
2020-11-13 03:06:25 -0500

    refactor practice 3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -2,9 +2,12 @@ import fetch from "node-fetch";
 
 export const getPoetry = () => {
   // fetch(poetry);
-  return fetch("https://v1.jinrishici.com/all.json").then((response) => {
-    return response.json().then((data) => {
-      return [data.origin, data.author, data.content];
-    });
-  });
+  return fetch("https://v1.jinrishici.com/all.json").then((response) =>
+    response
+      .json()
+      .then((data) => {
+        return [data.origin, data.author, data.content];
+      })
+      .catch(console.error)
+  );
 };
